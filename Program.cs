9ff5948a3246a271b865506090a2cbeddb07ec9a
unknown <khalidtobeprecise@gmail.com>
2026-09-25 15:05:53 +0400

    Testing to get the image from the git hub

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from .NET 10 - Version 3, get te image from the git hub ");
+app.MapGet("/", () => "Hello World from .NET 10 - Version 3, get te image from the git hub !");
 
 app.Run();
